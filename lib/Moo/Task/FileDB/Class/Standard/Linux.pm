c10c66359b0b27ba2dd52f208242212fb581172b
m <mmacnair.org>
2024-06-23 05:54:46 +0100

    Tidying

--- lib/Moo/Task/FileDB/Class/Standard/Linux.pm
+++ lib/Moo/Task/FileDB/Class/Standard/Linux.pm
@@ -1,10 +1,11 @@
 # ABSTRACT : Role assembly for common Linux Use Case
 package Moo::Task::FileDB::Class::Standard::Linux;
-our $VERSION = 'v0.0.10';
-##~ DIGEST : 420135969228e4a6283437658b8002bf
+our $VERSION = 'v0.0.11';
+##~ DIGEST : 15d4aa072503c2547d1733e298c1d880
 use Moo;
 with qw/
   Moo::Task::FileDB::Role::Core
+  Moo::Task::FileDB::Role::Linux
   Moo::GenericRole::DB::Working::AbstractSQLite
   Moo::Task::FileDB::Role::DB::SQLite::Setup
   Moo::Task::FileDB::Role::DB::AbstractSQLite
