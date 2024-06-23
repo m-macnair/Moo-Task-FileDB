cfd0ce3e4db7419fb348ead829f7371189442835
m <mmacnair.org>
2024-06-23 12:45:06 +0100

    removed use of rowid

--- lib/Moo/Task/FileDB/Role/DB/AbstractSQLite.pm
+++ lib/Moo/Task/FileDB/Role/DB/AbstractSQLite.pm
@@ -1,8 +1,8 @@
 
 # ABSTRACT : Do DB Things using SQLite + SQL Abstract
 package Moo::Task::FileDB::Role::DB::AbstractSQLite;
-our $VERSION = 'v0.0.10';
-##~ DIGEST : 91d30a06a1325b45d8729612e433d8b0
+our $VERSION = 'v0.0.11';
+##~ DIGEST : 682115a3c899c910521b20770f4b1c40
 use Moo::Role;
 
 #because I use confess everywhere
@@ -14,8 +14,8 @@ sub get_dir_id {
 	if ( $host ) {
 		$p->{host} = $host;
 	}
-	my $row = $self->select_insert_href( 'dirs', $p, [qw/* rowid/] );
-	return $row->{rowid};
+	my $row = $self->select_insert_href( 'dirs', $p, [qw/* /] );
+	return $row->{id};
 }
 
 sub get_file_id {
@@ -29,8 +29,8 @@ sub get_file_id {
 		name   => $file,
 		dir_id => $dir_id
 	};
-	my $row = $self->select_insert_href( 'files', $p, [qw/* rowid/] );
-	return $row->{rowid};
+	my $row = $self->select_insert_href( 'files', $p, [qw/* /] );
+	return $row->{id};
 }
 
 1;
